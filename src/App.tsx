62bf6aa97cddc39ad3cd7be8c1f4a013012fc977
luisflr <lfloresr@ulasalle.edu.pe>
2024-05-06 14:58:41 -0500

    [ADD] adding tailwind and creating simple view

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import BoldIcon from './assets/icons/BoldIcon'
+import IncraseTextIcon from './assets/icons/IncraseTextIcon'
+import ItalicIcon from './assets/icons/ItalicIcon'
+import UnderlineIcon from './assets/icons/UnderlineIcon'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const buttons = [
+    {
+      name: 'bold',
+      icon: <BoldIcon wrapperClassName='w-7 h-7'/> ,
+      label: 'Bold'
+    },
+    {
+      name: 'italic',
+      icon: <ItalicIcon wrapperClassName='w-7 h-7'/>,
+      label: 'Italic'
+    },
+    {
+      name: 'underline',
+      icon: <UnderlineIcon wrapperClassName='w-7 h-7'/>,
+      label: 'Underline'
+    },
+    {
+      name: 'increase-text',
+      icon: <IncraseTextIcon wrapperClassName='w-8 h-8 text-black'/>,
+      label: 'Increase Text'
+    },
+    // {name: 'decrease-text', icon: ''},
+    // {name: 'color', icon: ''},
+  ]
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <section className='h-screen w-full flex justify-center items-center'>
+      <div className='h-full flex flex-col items-center py-20 '>
+        <h1 className='text-4xl mb-12'> My First <span className='font-bold'>Text Editor </span></h1>
+        <div className='flex w-full items-center justify-center mb-6'>
+          {buttons.map(button =>
+          <div key={`button-${button.name}`} className='flex flex-col items-center w-[80px] h-[100px] text-center'> 
+            <button className='cursor-pointer'>
+              {button.icon}
+            </button>
+          </div>
+          )}
+        </div>
+        <textarea className='bg-[#F9F9F9] w-[600px] min-h-[200px] border-none outline-none p-4 rounded-lg'>
+
+        </textarea>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      
+    </section>
   )
 }
 
